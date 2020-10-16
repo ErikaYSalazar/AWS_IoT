ad70997c140c4cdf11264294e5bc21eeeaa25403
Erika Salazar <ErikaYSalazar@users.noreply.github.com>
2020-10-16 23:14:28 +0200

    Update sense_hat.py

--- scripts/python/accesories/sense_hat.py
+++ scripts/python/accesories/sense_hat.py
@@ -5,4 +5,4 @@ red = (255, 0, 0)
 white = (255, 255, 255)
 
 while True:
-  sense.show_message("IoT Hackaton!", text_colour=red, back_colour=white, scroll_speed=0.2)
+  sense.show_message("Hello World!", text_colour=red, back_colour=white, scroll_speed=0.2)
